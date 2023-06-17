9b5b9a52c51b2f6ffb81f9dd970ca3a3b67c1833
Ebenezer <agyekumebenezer2001@gmail.com>
2023-06-17 14:10:07 +0000

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDIp2sKLpBPRNKcuMF3dwLbUerR1I4yrs",
+  authDomain: "mediline-7b507.firebaseapp.com",
+  projectId: "mediline-7b507",
+  storageBucket: "mediline-7b507.appspot.com",
+  messagingSenderId: "643339151714",
+  appId: "1:643339151714:web:71aef9662c4e0da0a5643b",
+};
+
+export default firebaseConfig;
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB7BVq1m0GjE2KZMJGDa7kiJT_J04Tf_CM",
+//   authDomain: "foodapp-1e17e.firebaseapp.com",
+//   databaseURL: "https://foodapp-1e17e-default-rtdb.firebaseio.com",
+//   projectId: "foodapp-1e17e",
+//   storageBucket: "foodapp-1e17e.appspot.com",
+//   messagingSenderId: "1049061514642",
+//   appId: "1:1049061514642:web:2f43641e559cc778ff754b"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
